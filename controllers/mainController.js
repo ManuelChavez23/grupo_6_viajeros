49b71e89150deac598c5c5be95474dd4650ccf18
pimiento-2021 <manusabale@gmail.com>
2022-08-23 23:59:13 -0300

    server

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,6 @@
 const controller = {
     index: (req, res) =>{
-        res.sendFile(path.join(__dirname,'/views/index.html'))
+        res.sendFile(path.join(__dirname,'/views/index'))
     },
     contact: (req, res) =>{
         res.sendFile(path.join(__dirname,'/views/contacto.html'))
