ab1a986f5b85945ec72e1b3e7abd0ccf24e0d6b0
FrancoAmbort <ambortfranco94@gmail.com>
2022-09-07 20:40:27 -0300

    archivos JSON

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersJson = fs.readFileSync(path.join(__dirname, '../data/usersBd.json'), 'utf-8');
+
+const users = JSON.parse(usersJson);
+
+
 const userController = {
     login: (req, res) =>{
         res.render('login')
