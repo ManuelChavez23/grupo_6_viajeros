8791d3243651ea7e4543a983fd2b909623813dee
FrancoAmbort <ambortfranco94@gmail.com>
2022-09-13 11:08:43 -0300

    json

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,23 +10,11 @@ const controller = {
     
     detail:  (req, res) =>{
         let idProduct = req.params.id;
-<<<<<<< HEAD
         
         products.find( productActual => productActual.id == idProduct)
        
         res.render('productDetail', {products});
-=======
 
-        products.forEach(productActual => {
-            if (productActual.id === idProduct) {
-                productActual.name = req.body.name,
-                productActual.price = req.body.price,
-                productActual.detail = req.body.detail,
-                productActual.img = req.body.img
-            }
-        })
-        res.render('productDetail', {products, idProduct});
->>>>>>> e97a83602007a3e809efcf984e00b463338d0d1e
     },
     products:  (req, res) =>{
         res.render('products', {products});
