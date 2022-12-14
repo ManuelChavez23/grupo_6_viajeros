1060ecf025f7ecb968ab67e8b28a8326ac007682
PamelaAbeldanio <pbeldanio@gmail.com>
2022-12-14 15:11:19 -0300

    Diseno Dashboard en grupo

--- frontend2/frontend/src/components/GenresInDb.js
+++ frontend2/frontend/src/components/GenresInDb.js
@@ -1,4 +1,10 @@
 import React from "react";
+import imagenFondoFilipinas from '../assets/images/filipinas3.jpeg';
+import imagenFondoGrecia from '../assets/images/grecia1.jpg';
+import imagenFondoEspana from '../assets/images/españa.jpeg';
+import imagenFondoRusia from '../assets/images/rusia2.jpeg';
+import imagenFondoMexico from '../assets/images/mexico.jpeg';
+import imagenFondoTailandia from '../assets/images/tailandia.jpeg';
 
 function GenresInDb() {
   return (
@@ -6,62 +12,81 @@ function GenresInDb() {
       <div className="card shadow mb-4">
         <div className="card-header py-3">
           <h5 className="m-0 font-weight-bold text-gray-800">
-            Genres in Data Base
+            Viajes destacados
           </h5>
         </div>
         <div className="card-body">
           <div className="row">
             <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow" >
+              <img className="card-img" src={imagenFondoFilipinas}/>
+              <div class="card-img-overlay">
+                <div className="card-body">Filipinas</div>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
-                <div className="card-body">Acción</div>
+              <img className="card-img"  src={imagenFondoGrecia} />
+              <div class="card-img-overlay">
+                <div className="card-body">Grecia</div>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+              <img className="card-img" src={imagenFondoEspana} />
+              <div class="card-img-overlay">
+                <div className="card-body">España</div>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+              <img className="card-img" src={imagenFondoRusia} />
+              <div class="card-img-overlay">
+                <div className="card-body">Rusia</div>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+              <img className="card-img" src={imagenFondoMexico} />
+              <div class="card-img-overlay">
+                <div className="card-body">Mexico</div>
+              </div>
+            </div>
+            </div>
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+              <img className="card-img" src={imagenFondoTailandia} />
+              <div class="card-img-overlay">
+                <div className="card-body">Tailandia</div>
+              </div>
+            </div>
+            </div>
+            {/*
+            <div className="col-lg-6 mb-4">
+              <div className="card bg-dark text-white shadow">
+                <div className="card-body">Portugal</div>
               </div>
             </div>
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
-                <div className="card-body">Animación</div>
+                <div className="card-body">Canada</div>
               </div>
             </div>
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
-                <div className="card-body">Aventura</div>
+                <div className="card-body">Filipinas</div>
               </div>
             </div>
             <div className="col-lg-6 mb-4">
               <div className="card bg-dark text-white shadow">
-                <div className="card-body">Ciencia Ficción</div>
+                <div className="card-body">Haiti</div>
               </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Comedia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Documental</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Drama</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Fantasia</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Infantiles</div>
-              </div>
-            </div>
-            <div className="col-lg-6 mb-4">
-              <div className="card bg-dark text-white shadow">
-                <div className="card-body">Musical</div>
-              </div>
-            </div>
-          </div>
+            </div>*/}
+          </div> 
         </div>
       </div>
     </div>
