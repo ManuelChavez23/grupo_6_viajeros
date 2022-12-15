1f8f2a1fe05131771d6af3f22bd53a4a6fcb8181
FrancoAmbort <ambortfranco94@gmail.com>
2022-12-15 20:09:44 -0300

    search en grupo

--- frontend2/frontend/src/components/SearchMovies.js
+++ frontend2/frontend/src/components/SearchMovies.js
@@ -4,20 +4,20 @@ import React, { useState, useEffect, useRef } from 'react';
  */
 function SearchMovies(){
 
-	const[movies, setMovies] = useState([]);
-	const [keyword, setKeyword] = useState('Action');
+	const[destinys, setDesnitys] = useState([]);
+	const [keyword, setKeyword] = useState('');
 	const busqueda = useRef(null)
 
 	
 
 	// Credenciales de API
-	const apiKey = 'c53f759'; // Intenta poner cualquier cosa antes para probar
+	/* const apiKey = 'c53f759'; */ // Intenta poner cualquier cosa antes para probar
 
 	useEffect(() => {
-		fetch(`https://www.omdbapi.com/?s=${keyword}&apikey=${apiKey}`)
+		fetch(`http://localhost:3001/api/products`)
 			.then(response => response.json())
 			.then(data => {
-				setMovies(data.Search)
+				setDesnitys(data.data)
 			})
 	}, [keyword])
 
@@ -34,7 +34,7 @@ function SearchMovies(){
 	return(
 		<div className="container-fluid">
 			{
-				apiKey !== '' ?
+				/* apiKey !== '' ? */
 				<>
 					<div className="row my-4">
 						<div className="col-12 col-md-6">
@@ -50,27 +50,27 @@ function SearchMovies(){
 					</div>
 					<div className="row">
 						<div className="col-12">
-							<h2>Películas para la palabra: {keyword}</h2>
+							<h2>Destinos para la palabra: {keyword}</h2>
 						</div>
 						{/* Listado de películas */}
 						{
-							movies.length > 0 && movies.map((movie, i) => {
+							destinys.length > 0 && destinys.map((destiny, i) => {
 								return (
 									<div className="col-sm-6 col-md-3 my-4" key={i}>
 										<div className="card shadow mb-4">
 											<div className="card-header py-3">
-												<h5 className="m-0 font-weight-bold text-gray-800">{movie.Title}</h5>
+												<h5 className="m-0 font-weight-bold text-gray-800">{destiny.name}</h5>
 											</div>
 											<div className="card-body">
 												<div className="text-center">
 													<img 
 														className="img-fluid px-3 px-sm-4 mt-3 mb-4" 
-														src={movie.Poster}
-														alt={movie.Title} 
+														src={destiny.img}
+														alt="" 
 														style={{ width: '90%', height: '400px', objectFit: 'cover' }} 
 													/>
 												</div>
-												<p>{movie.Year}</p>
+												<p>{destiny.price}</p>
 											</div>
 										</div>
 									</div>
@@ -78,10 +78,10 @@ function SearchMovies(){
 							})
 						}
 					</div>
-					{ movies.length === 0 && <div className="alert alert-warning text-center">No se encontraron películas</div>}
+					{ destinys.length === 0 && <div className="alert alert-warning text-center">No se encontraron películas</div>}
 				</>
-				:
-				<div className="alert alert-danger text-center my-4 fs-2">Eyyyy... ¿PUSISTE TU APIKEY?</div>
+				/* :
+				<div className="alert alert-danger text-center my-4 fs-2">Eyyyy... ¿PUSISTE TU APIKEY?</div> */
 			}
 		</div>
 	)
