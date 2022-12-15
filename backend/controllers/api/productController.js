f11866df7ab53a686969368233a93c826faa2cfd
PamelaAbeldanio <pbeldanio@gmail.com>
2022-12-14 21:30:41 -0300

    api/products en grupo

--- backend/controllers/api/productController.js
+++ backend/controllers/api/productController.js
@@ -4,23 +4,27 @@ const sequelize = db.sequelize;
 
 const controller = {
     destiny: (req,res) => {
-        db.Destiny.findAll({raw:true, attributes: ['id', 'name', 'detail']}) 
+        db.Destiny.findAll({
+                raw:true,
+                nest:true,
+                attributes: ['id', 'name', 'detail']
+            }) 
         .then(destinys => {
             destinys = destinys.map(destiny => ({
                 ...destiny,
                 productDescription: `/api/products/${destiny.id}`,
-               /*  countByCategory: destiny.destiny_category_id, */
-            }))
+        }))
             res.json({
                 count: destinys.length,
                 data: destinys,
-                status: 200,
+                status: 200
             })
         })
         
     },
     destinyId: (req, res) => {
-        db.Destiny.findByPk(req.params.id, {raw:true})
+        db.Destiny.findByPk(req.params.id, {raw:true, nest:true,
+            include: ['transports', 'meals', 'salidas', 'categorys', 'anuncios']})
         .then(destinyId => {
             res.json({
                 ...destinyId,
