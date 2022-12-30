8fcbbe89238b6997f86615c1e902df2968c739a9
PamelaAbeldanio <pbeldanio@gmail.com>
2022-12-30 12:40:23 -0300

    commit vacio

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,7 +1,8 @@
 function adminMiddleware (req, res, next) {
+    if(req.session.usuariologueado){
     if(req.session.usuariologueado.user_category_id !== 2) {
         res.redirect('/')
-    }
+    }}
 
     next();
 }
